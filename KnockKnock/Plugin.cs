581ece1e574eb951cf427a254f017ae411a5ddbe
Dominik Rutter <anastasia.v.rutter@gmail.com>
2025-03-14 19:28:08 -0600

    Setting up project to use all the right names

--- KnockKnock/Plugin.cs
+++ KnockKnock/Plugin.cs
@@ -6,7 +6,7 @@ using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
 
-namespace SamplePlugin;
+namespace KnockKnock;
 
 public sealed class Plugin : IDalamudPlugin
 {
