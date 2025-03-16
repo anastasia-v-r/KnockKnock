1ef450ec7340dd97fa07a39538984497940d04b9
Dominik Rutter <anastasia.v.rutter@gmail.com>
2025-03-15 23:10:47 -0600

    create a wrapper for sheet access for simplification

--- KnockKnock/SheetsWrapper.cs
+++ KnockKnock/SheetsWrapper.cs
@@ -1,12 +1,28 @@
+using SamplePlugin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lumina.Excel;
+using Lumina.Excel.Sheets;
 
 namespace KnockKnock;
 
-public class SheetsWrapper
+public static class SheetsWrapper
 {
-    
+    private static ExcelSheet<World>? worldSheet = DalamudServices.DataManager.GetExcelSheet<World>();
+
+    public static string? GetWorldFromId(int id)
+    {
+        if (worldSheet == null) return null;
+        foreach (World world in worldSheet)
+        {
+            if (world.RowId == id)
+            {
+                return world.Name.ExtractText();
+            }
+        }
+        return null;
+    }
 }
