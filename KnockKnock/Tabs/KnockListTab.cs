4ea7ec21039d7b928e109b8bc9c4af1f53ecc6a0
Dominik Rutter <anastasia.v.rutter@gmail.com>
2025-03-16 21:25:33 -0600

    Clean up the look of the player list by using the table for storing the players and the removal/toggle functionality

--- KnockKnock/Tabs/KnockListTab.cs
+++ KnockKnock/Tabs/KnockListTab.cs
@@ -30,55 +30,64 @@ public class KnockListTab : ITab
         if (ImGui.BeginTabItem(Name))
         {
             // Setup headers for play list table
-            if (ImGui.BeginTable("Table", 4, ImGuiTableFlags.Borders))
+                if (ImGui.BeginTable("Table", 4, ImGuiTableFlags.None))
             {
                 // Setup headers for table
                 DalamudServices.Log.Debug($"{Misc.TEXT_BASE_WIDTH}");
-                ImGui.TableSetupColumn("Enable", ImGuiTableColumnFlags.WidthFixed, Misc.TEXT_BASE_WIDTH * 5);
+                ImGui.TableSetupColumn("Enable", ImGuiTableColumnFlags.WidthFixed, Misc.TEXT_BASE_WIDTH * 7);
                 ImGui.TableSetupColumn("Player Name", ImGuiTableColumnFlags.WidthFixed, Misc.TEXT_BASE_WIDTH * 25);
                 ImGui.TableSetupColumn("Homeworld", ImGuiTableColumnFlags.WidthFixed, Misc.TEXT_BASE_WIDTH * 15);
                 ImGui.TableSetupColumn("Friend?", ImGuiTableColumnFlags.WidthFixed, Misc.TEXT_BASE_WIDTH * 5);
                 //ImGui.PopItemWidth();
                 ImGui.TableHeadersRow();
 
+                // Display current list of players on whitelist
+                var UserList = Configuration.StoredPlayers;
+                if (UserList.Count > 0)
+                {
+                    // Run through existing player list and display their info as well as provide the ability to remove them
+                    foreach (var player in UserList)
+                    {
+                        ImGui.TableNextRow();
+
+                        // Adds button for removal from whitelist
+                        ImGui.TableSetColumnIndex(0);
+                        ImGui.SetNextItemWidth(Misc.TEXT_BASE_WIDTH * 2.5f);
+                        if (ImGui.ArrowButton($"##RemoveNameButton {player.Key}", ImGuiDir.Left)) 
+                        {
+                            UserList.Remove(player.Key);
+                            Configuration.StoredPlayers = UserList;
+                            Configuration.Save();
+                            break;
+                        }
+                        
+                        // Allows user to toggle whitelisted user temporarily
+                        ImGui.SameLine();
+                        ImGui.SetNextItemWidth(Misc.TEXT_BASE_WIDTH * 2.5f);
+                        var isEnabled = true;
+                        if (ImGui.Checkbox("##Enable/Disable Player", ref isEnabled)) ;
+
+                        // Displays the whitelisted players name
+                        ImGui.TableSetColumnIndex(1);
+                        ImGui.Text(player.Value.Name);
+
+                        // Displays the whitelisted players homeworld
+                        ImGui.TableSetColumnIndex(2);
+                        ImGui.Text(player.Value.HomeWorld.ToString());
+
+                        // Shows if whitelisted player is on the users friendlist
+                        ImGui.TableSetColumnIndex(3);
+                        ImGui.BeginDisabled();
+                        var isFriend = player.Value.IsPlayerFriend;
+                        if (ImGui.Checkbox("##isfriend", ref isFriend))
+                        {
+
+                        }
+                        ImGui.EndDisabled();
+                    }
+                }
 
                 ImGui.EndTable();
-            }
-            
-
-            // Display current list of players on whitelist
-            var UserList = Configuration.StoredPlayers;
-            if (UserList.Count > 0)
-            {
-                foreach (var player in UserList)
-                {
-                    if (ImGui.ArrowButton($"##RemoveNameButton {player.Key}", ImGuiDir.Left))
-                    {
-                        UserList.Remove(player.Key);
-                        Configuration.StoredPlayers = UserList;
-                        Configuration.Save();
-                        break;
-                    }
-
-                    ImGui.SameLine(Misc.TEXT_BASE_WIDTH * 5f);
-                    var isEnabled = true;
-                    if (ImGui.Checkbox("##Enable/Disable Player", ref isEnabled));
-
-                    ImGui.SameLine(Misc.TEXT_BASE_WIDTH * (25f + 5f));
-                    ImGui.Text(player.Value.Name);
-
-                    ImGui.SameLine(Misc.TEXT_BASE_WIDTH * ((25f + 5f) + 15f));
-                    ImGui.Text(player.Value.HomeWorld.ToString());
-
-                    ImGui.SameLine(Misc.TEXT_BASE_WIDTH * (((25f + 5f) + 15f) + 5f));
-                    ImGui.BeginDisabled();
-                    var isFriend = player.Value.IsPlayerFriend;
-                    if (ImGui.Checkbox("##isfriend", ref isFriend))
-                    {
-
-                    }
-                    ImGui.EndDisabled();
-                }
             }
             ImGui.EndTabItem();
         }
