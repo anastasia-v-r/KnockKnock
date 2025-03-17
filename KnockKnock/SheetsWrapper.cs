42d891a26e9fcad35873ed6a95987c0444c07ca8
Dominik Rutter <anastasia.v.rutter@gmail.com>
2025-03-16 21:12:42 -0600

    add more comments/documents

--- KnockKnock/SheetsWrapper.cs
+++ KnockKnock/SheetsWrapper.cs
@@ -9,10 +9,14 @@ using Lumina.Excel.Sheets;
 
 namespace KnockKnock;
 
+// Create a wrapper for sheets access to simplify things a bit
 public static class SheetsWrapper
 {
+    // Store needed sheets
     private static ExcelSheet<World>? worldSheet = DalamudServices.DataManager.GetExcelSheet<World>()!;
 
+
+    // Allow user to be able to covert world id to a world name
     public static string? GetWorldFromId(uint worldId)
     {
         if (worldSheet.TryGetRow(worldId, out var world))
